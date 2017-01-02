df04b98f1be3a6e9f57e6bb823d04736fda622c8
AlvinL33 <alee2@stbens.org.uk>
2017-01-02 18:53:57 +0000

    Add files via upload

---
+++ magicMirror/CalendarModule.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'MagicMirror'
+
+# Gets user creds from file
+def getCredentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: 
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def getCalEvents():
+    # Lists next 5 upcoming events
+    credentials = getCredentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=5, singleEvents=True,
+        orderBy='startTime').execute()
+    return eventsResult.get('items', [])
+
+if __name__ == '__main__':
+    getCalEvents()
---
+++ magicMirror/readMe.txt
@@ -0,0 +1,8 @@
+MAGIC MIRROR BY ALVIN LEE
+
+Without adding a Wunderground key in 'WeatherModule.py' this software will not work.
+
+I removed mine because GitHub is public.
+I have also removed my Google account information from the app.
+
+Sorry for the inconvenience caused
